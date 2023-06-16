5de00e762046e0fe86cd4e4b5aa78efc980a833a
Jakub Vasicek <j.vasicek58@gmail.com>
2023-06-16 14:58:34 +0200

    config-building GUI

---
+++ docs/configBuilder.js
@@ -0,0 +1,62 @@
+
+function setGeneralParams(ensemblRelease, custom_tr_list, transcript_biotypes, final_fasta_path) {
+    const general_config_html = "<p>\
+        # ---------------- Global parameters ----------------<br>" +
+        "Ensembl_FTP_URL: ftp.ensembl.org/pub/release-" + ensemblRelease + "/<br>" +
+        "annotationFilename: Homo_sapiens.GRCh38." + ensemblRelease + ".chr_patch_hapl_scaff<br>" +
+        "<br>" +
+        "custom_transcript_list: \"" + custom_tr_list + "\"<br>"+
+        "included_transcript_biotypes: \"" + transcript_biotypes + "\"<br>" +
+        "<br>" +
+        "final_fasta_file: " + (final_fasta_path.length == 0 ? "MISSING" :  "\"" + final_fasta_path + "\"") + "<br>" +
+        "<\p>"
+
+    return general_config_html
+}
+
+function setProVarParams(include_variants, VCF_data, var_require_start, var_fasta_file, var_table_file) {
+    let vcf_datasets = ""
+
+    VCF_data.forEach(element => {
+        vcf_datasets += '&nbsp;&nbsp;' + element[0].replace(' ', '_') + ': { file: \"' + element[1] + "\", fasta_accession_prefix: " + element[0].replace(' ', '_') + '_' + ", min_af: " + element[2] + " }<br>"
+    });
+
+    const provar_config_html = "<p>\
+        # ---------------- ProVar parameters ----------------<br>" +
+        "include_var: " + (include_variants ? "True" : "False") + '<br>' +
+        (VCF_data.length === 0 ? "variant_vcf: {}<br>" : "variant_vcf: <br>" + vcf_datasets) +
+        "<br>" + 
+        "working_dir_name_var: variants_tmp<br><br>" +
+        "var_require_start: " + (var_require_start ? "1" : "0") + '<br>' +
+        "<br>" +
+        "var_fasta_file: " + (var_fasta_file.length == 0 ? "MISSING" :  "\"" + var_fasta_file + "\"") + "<br>" +
+        "var_table_file: " + (var_table_file.length == 0 ? "MISSING" :  "\"" + var_table_file + "\"") + "<br>" +
+        "</p>"
+
+    return provar_config_html
+}
+
+function setProHapParams(include_haplotypes, dataset_url, vcf_filename, freq_threshold, count_threshold, MAF_threshold, haplo_require_start, haplo_ignore_UTR, haplo_skip_start_lost, haplo_fasta_file, haplo_table_file) {
+    const prohap_config_html = "<p>\
+    # ---------------- ProHap parameters ----------------<br>" +
+    "include_haplo: " + (include_haplotypes ? "True" : "False") + '<br>' +
+    "<br>" +
+    "1kGP_FTP_URL: " + dataset_url + '<br>' +
+    "1kGP_vcf_file_name: " + vcf_filename + '<br>' +
+    "<br>" +
+    "working_dir_name_haplo: haplotypes_tmp<br><br>" +
+    "haplo_min_freq: " + freq_threshold + "<br>" +
+    "haplo_min_count: " + count_threshold + "<br>" +
+    "1kGP_min_af: " + MAF_threshold + "<br>" +
+    "<br>" +
+    "haplo_require_start: " + (haplo_require_start ? "1" : "0") + "<br>" +
+    "haplo_ignore_UTR: " + (haplo_ignore_UTR ? "1" : "0") + "<br>" +
+    "haplo_skip_start_lost: " + (haplo_skip_start_lost ? "1" : "0") + "<br>" +
+    "max_cores: 3<br>" +
+    "<br>" +
+    "haplo_fasta_file: " + (haplo_fasta_file.length == 0 ? "MISSING" :  "\"" + haplo_fasta_file + "\"") + "<br>" +
+    "haplo_table_file: " + (haplo_table_file.length == 0 ? "MISSING" :  "\"" + haplo_table_file + "\"") + "<br>" +
+    "</p>"
+
+    return prohap_config_html
+}
