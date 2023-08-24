cf0de07314d59a441298f6872ec0f09c07b17756
Jakub Vasicek <j.vasicek58@gmail.com>
2023-08-24 11:20:41 +0200

    Fix removing UTR sequences

--- src/remove_UTR_seq.py
+++ src/remove_UTR_seq.py
@@ -16,10 +16,18 @@ all_proteins = read_fasta(args.input_file)
 outfile = open(args.output_file, 'w')
 
 for protein in all_proteins.values():
-	proteinIDs = protein['description'].split('protein_IDs:', 1)[1].split(maxsplit=1)[0].split(';')
-	is_UTR = all([ 'UTR' in id for id in proteinIDs ])
-	if not is_UTR:
-		outfile.write('>' + protein['tag'] + '|' + protein['accession'] + '|' + protein['description'] + '\n')
+	splitIDs = protein['description'].split('split_sequences:', 1)[1].split(maxsplit=1)[0].split(';')
+
+	# remove information for proteins that contain only their UTR region in this sequence
+	proteinIDs = [ x for x,i in enumerate(protein['description'].split('matching_proteins:', 1)[1].split(maxsplit=1)[0].split(';')) if ('UTR' not in splitIDs[i]) ]
+	proteinPos = [ x for x,i in enumerate(protein['description'].split('position_within_protein:', 1)[1].split(maxsplit=1)[0].split(';')) if ('UTR' not in splitIDs[i]) ]
+	proteinStart = [ x for x,i in enumerate(protein['description'].split('start:', 1)[1].split(maxsplit=1)[0].split(';')) if ('UTR' not in splitIDs[i]) ]
+	proteinRF = [ x for x,i in enumerate(protein['description'].split('reading_frame:', 1)[1].split(maxsplit=1)[0].split(';')) if ('UTR' not in splitIDs[i]) ]
+
+	if (len(proteinIDs) > 0):
+		# remove the split sequence IDs from the description - they are redundant for the search and PSM annotation
+		description = 'position_within_protein:' + ';'.join(proteinPos) + ' start:' + ';'.join(proteinStart) + ' matching_proteins:' + ';'.join(proteinIDs) + ' reading_frame:' + ';'.join(proteinRF)
+		outfile.write('>' + protein['tag'] + '|' + protein['accession'] + '|' + description + '\n')
 		outfile.write(protein['sequence'] + '\n')
 
 outfile.close()
